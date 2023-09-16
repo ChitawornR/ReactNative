d74a6af179542a895bbbd6c82117f4f6e0ab987a
ChitawornR <chitaworn.rr@gmail.com>
2023-09-16 10:34:10 +0700

    CustomSideBarMenu

--- App_drawer_with_stack.js
+++ App_drawer_with_stack.js
@@ -1,20 +1,21 @@
 import 'react-native-gesture-handler';
-import { View, Text, Button} from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createDrawerNavigator} from '@react-navigation/drawer'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
-
 import FirstPage from './page/FirstPage'
 import SecondPage from './page/SecondPage'
-import ThirdPage from './page/ThirdPage'
-import CustomSideBarMenu from './page/CustomSideBarMenu';
 
 
 function FirstDrawer(){
     return (
         <Stack.Navigator
-         screenOptions={{headerShown:false}}
+        screenOptions={{
+            headerStyle:{
+                backgroundColor: '#FFC0CB'
+            }
+         }}
         >
             <Stack.Screen name= 'First' component={FirstPage} options={{title: 'First Page'}}/>
         </Stack.Navigator>
@@ -24,7 +25,11 @@ function FirstDrawer(){
 function SecondDrawer(){
     return(
         <Stack.Navigator
-         screenOptions={{headerShown:false}}
+         screenOptions={{
+            headerStyle:{
+                backgroundColor: '#FFC0CB'
+            }
+         }}
         >
             <Stack.Screen name='Second' component={SecondPage} options={{title: 'Second Page'}}/>
         </Stack.Navigator>
@@ -43,7 +48,6 @@ function MyDrawer(){
                 width: 240,
               },
             }}
-            drawerContent={(props)=> <CustomSideBarMenu{...props}/>}
         >
             <Drawer.Screen name='FirstDrawer' component={FirstDrawer} options={{drawerLabel: 'First Page Option'}}/>
             <Drawer.Screen name='SecondDrawer' component={SecondDrawer} options={{drawerLabel: 'Second Page'}}/>
